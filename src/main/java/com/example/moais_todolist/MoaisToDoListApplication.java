53edd0e7785c339b7a1c8181f728762601945314
엽킹 <kim91star2@naver.com>
2022-05-03 21:55:57 +0900

    Initial commit

---
+++ src/main/java/com/example/moais_todolist/MoaisToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.moais_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaisToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaisToDoListApplication.class, args);
+    }
+
+}
